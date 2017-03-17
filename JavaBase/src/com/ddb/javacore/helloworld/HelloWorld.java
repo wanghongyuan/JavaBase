f5a5bd42f80062c006d42d72fc52d990bff01ce3
why <917098302@qq.com>
2017-03-17 17:07:56 +0800

    this is my first commit
    Signed-off-by: why <917098302@qq.com>

---
+++ JavaBase/src/com/ddb/javacore/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ddb.javacore.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ JavaBase/src/com/ddb/javacore/helloworld/copy/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ddb.javacore.helloworld.copy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+
+	}
+
+}
